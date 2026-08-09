7f787da663c1a6e578173d93d7191d500ded9c93
asiostyx-create <asio.styx@gmail.com>
2026-08-09 23:55:41 +0300

    Add files via upload

--- src/main/java/com/example/notification_service/EmailService.java
+++ src/main/java/com/example/notification_service/EmailService.java
@@ -3,6 +3,7 @@ package com.example.notification_service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
@@ -12,29 +13,35 @@ public class EmailService {
 
     private static final Logger log = LoggerFactory.getLogger(EmailService.class);
     private final JavaMailSender mailSender;
+    private String from;
+    private String host;
 
-    public EmailService(JavaMailSender mailSender) {
+    public EmailService(JavaMailSender mailSender,
+                        @Value("${app.mail.from}") String from,
+                        @Value("${app.host}") String host) {
         this.mailSender = mailSender;
+        this.from = from;
+        this.host = host;
     }
 
     public void sendCreatedNotification(String username, String email) {
         String subject;
         String text;
         subject = "Вы зарегистрировались на сайте";
-        text = String.format("Здравствуйте, %s! Ваш аккаунт на сайте \"сайт\" был успешно создан.", username);
+        text = String.format("Здравствуйте! Ваш аккаунт на сайте %s был успешно создан.", host);
         sendEmail(email, subject, text);
     }
     public void sendDeletedNotification(String username, String email) {
         String subject;
         String text;
         subject = "Удаление аккаунта";
-        text = String.format("Здравствуйте, %s! Ваш аккаунт на сайте \"сайт\" был удален.", username);
+        text = String.format("Здравствуйте! Ваш аккаунт был удален.");
         sendEmail(email, subject, text);
     }
 
     public void sendEmail(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("pochta@pochta.ru");
+        message.setFrom(from);
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
